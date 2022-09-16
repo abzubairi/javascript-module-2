b9a04b3627a746cdd343fa52a6bae837631592ba
Hafiz Abubaker Nazir <habubakernazir@gmail.com>
2022-09-17 01:29:47 +0200

    initial

---
+++ week-1/Homework/dom-manipulation-2/js/main.js
@@ -0,0 +1,37 @@
+const blueButton=document.querySelector("#blueBtn");
+
+blueButton.addEventListener("click", function() {
+    let myElement = document.querySelector(".jumbotron");
+myElement.style.backgroundColor = "#588fbd";
+
+const donateBike = document.querySelector(".btn-primary");
+donateBike.style.backgroundColor = "#ffa500";
+const volunteer = document.querySelector(".btn-secondary");
+volunteer.style.backgroundColor = "black";
+volunteer.style.color = "white";
+});
+const orangeButton=document.querySelector("#orangeBtn");
+
+orangeButton.addEventListener("click", function() {
+    let myElement = document.querySelector(".jumbotron");
+myElement.style.backgroundColor = "#f0ad4e";
+
+const donateBike = document.querySelector(".btn-primary");
+donateBike.style.backgroundColor = "#5751fd";
+const volunteer = document.querySelector(".btn-secondary");
+volunteer.style.backgroundColor = "#31b0d5";
+volunteer.style.color = "white";
+});
+
+const greenButton=document.querySelector("#greenBtn");
+
+greenButton.addEventListener("click", function() {
+    let myElement = document.querySelector(".jumbotron");
+myElement.style.backgroundColor = "#87ca8a";
+
+const donateBike = document.querySelector(".btn-primary");
+donateBike.style.backgroundColor = "black";
+const volunteer = document.querySelector(".btn-secondary");
+volunteer.style.backgroundColor = "#8c9c08";
+
+});
