d936bf82df2c8fb05566bc545bc0a07a177f4cf1
Hafiz Abubaker Nazir <habubakernazir@gmail.com>
2022-09-18 23:27:08 +0200

    done

--- week-1/Homework/dom-manipulation-2/js/main.js
+++ week-1/Homework/dom-manipulation-2/js/main.js
@@ -4,9 +4,10 @@ blueButton.addEventListener("click", function() {
     let myElement = document.querySelector(".jumbotron");
 myElement.style.backgroundColor = "#588fbd";
 
-const donateBike = document.querySelector(".btn-primary");
+const donateBike = document.querySelector("#content .btn-primary");
 donateBike.style.backgroundColor = "#ffa500";
 const volunteer = document.querySelector(".btn-secondary");
+
 volunteer.style.backgroundColor = "black";
 volunteer.style.color = "white";
 });
@@ -16,7 +17,7 @@ orangeButton.addEventListener("click", function() {
     let myElement = document.querySelector(".jumbotron");
 myElement.style.backgroundColor = "#f0ad4e";
 
-const donateBike = document.querySelector(".btn-primary");
+const donateBike = document.querySelector("#content .btn-primary");
 donateBike.style.backgroundColor = "#5751fd";
 const volunteer = document.querySelector(".btn-secondary");
 volunteer.style.backgroundColor = "#31b0d5";
@@ -29,9 +30,24 @@ greenButton.addEventListener("click", function() {
     let myElement = document.querySelector(".jumbotron");
 myElement.style.backgroundColor = "#87ca8a";
 
-const donateBike = document.querySelector(".btn-primary");
+const donateBike = document.querySelector("#content .btn-primary");
 donateBike.style.backgroundColor = "black";
 const volunteer = document.querySelector(".btn-secondary");
 volunteer.style.backgroundColor = "#8c9c08";
 
 });
+
+const formSubmit=document.querySelector(".form-group .btn-primary");
+formSubmit.addEventListener("submit", function(){
+    const inputCheck = document.querySelector(".form-control").value
+    const EmailCheck = document.querySelector("#exampleInputEmail1")
+    if(EmailCheck.contains("@") && inputCheck== "")
+    return true;
+    else{
+        EmailCheck.style.backgroundColor="red";
+    }
+ 
+
+
+}
+)
