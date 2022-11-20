d9cb527ae9d99439143a8fa2318f196eadb50901
Hafiz Abubaker Nazir <habubakernazir@gmail.com>
2022-11-20 22:16:37 +0100

    done

--- week-3/Homework/C-alarmclock/alarmclock.js
+++ week-3/Homework/C-alarmclock/alarmclock.js
@@ -1,37 +1,38 @@
 function setAlarm() {
-  const getValue = document.querySelector("input");
-  var setValue = 0;
-  setValue = getValue.value;
-  const timeRem = document.getElementById("timeRemaining");
-  timeRem.innerHTML = setValue;
-
-  setInterval(function () {
-    if (setValue != 0) {
-      timeRem.innerHTML = setValue--;
+  console.log("Setting Alarm...");
+  document.body.style.backgroundColor = "white";
+  let setTime = document.querySelector("#alarmSet").value.trim();
+  let timeRemaining = document.querySelector("#timeRemaining");
+  console.log(setTime);
+  const interval = setInterval(() => {
+    timeRemaining.textContent = `Time Remaining: ${new Date(
+      (setTime - 1) * 1000
+    )
+      .toISOString()
+      .substr(14, 5)}`;
+    if (setTime === 1) {
+      document.body.style.backgroundColor = "red";
+      playAlarm();
+      clearInterval(interval);
     }
+    // setTime--;
+    setTime = setTime - 1;
   }, 1000);
 }
-
 // DO NOT EDIT BELOW HERE
-
 var audio = new Audio("alarmsound.mp3");
-
 function setup() {
   document.getElementById("set").addEventListener("click", () => {
     setAlarm();
   });
-
   document.getElementById("stop").addEventListener("click", () => {
     pauseAlarm();
   });
 }
-
 function playAlarm() {
   audio.play();
 }
-
 function pauseAlarm() {
   audio.pause();
 }
-
 window.onload = setup;
--- week-3/Homework/D-slideshow/slideshow.js
+++ week-3/Homework/D-slideshow/slideshow.js
@@ -1,1 +1,13 @@
 // Write your code here
+
+setInterval(() => {
+  let newIndex = setNextIndex(findIndex());
+  showImage(newIndex);
+}, 10000);
+
+autoPreviousBtn.addEventListener("click", () => {
+  autoPrevious();
+});
+autoNextBtn.addEventListener("click", () => {
+  autoNext();
+});
